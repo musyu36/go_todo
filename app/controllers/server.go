6daf7d012a1831f28c6fbfe86bc8678f47bcd606
mimura <musyu36@gmail.com>
2021-04-13 08:03:36 +0900

    feat: ルーティング追加

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -26,6 +26,7 @@ func StartMainServer() error {
 	// URL登録(第１引数にURL , 第２引数にハンドラを登録)
 	// "/" にアクセスすると top を実行する、(topはroute_mainに記述済み)
 	http.HandleFunc("/", top)
+	http.HandleFunc("/signup", signup)
 	// サーバ立ち上げ
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
