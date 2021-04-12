c59ac67abbd3504780f1c6bdd0181bc40a09e517
mimura <musyu36@gmail.com>
2021-04-12 15:36:08 +0900

    chore: Bootstrap, jQuery 設定

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -6,6 +6,9 @@ import (
 )
 
 func StartMainServer() error {
+	files := http.FileServer(http.Dir(config.Config.Static)) // 静的ファイルの読み込み
+	// URLの設定、static という階層に設定にcss, js を設定したいが、実際には無いので、StripPrefix で static を取る
+	http.Handle("/static/", http.StripPrefix("/static/", files))
 	// URL登録(第１引数にURL , 第２引数にハンドラを登録)
 	// "/" にアクセスすると top を実行する、(topはroute_mainに記述済み)
 	http.HandleFunc("/", top)
