e21763ae2249d8680dad54f621cb0b1c7c9f5d9a
mimura <musyu36@gmail.com>
2021-04-12 11:14:42 +0900

    chore: config 記述

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"golang/todo_app/config"
+)
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+	fmt.Println(config.Config.LogFile)
+}
