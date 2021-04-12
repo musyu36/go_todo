b57d27fac6d20c315139324f3eda088d1ef7d22f
mimura <musyu36@gmail.com>
2021-04-12 15:46:23 +0900

    chore: レイアウトの共通化

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -1,9 +1,23 @@
 package controllers
 
 import (
+	"fmt"
 	"golang/todo_app/config"
 	"net/http"
+	"text/template"
 )
+
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+
+	// filesを可変長引数として渡す
+	templates := template.Must(template.ParseFiles(files...))
+	// layout templateを明示的に使用
+	templates.ExecuteTemplate(w, "layout", data)
+}
 
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static)) // 静的ファイルの読み込み
